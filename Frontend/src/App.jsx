30107669f2a2f5264a75592b1f75884943993f92
shreyassiu <22ucs205@lnmiit.ac.in>
2025-02-04 02:15:48 +0530

    kjb

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [shortUrl, setShortUrl] = useState('')
   const [loading, setLoading] = useState(false)
 
-  axios.defaults.baseURL = 'http://localhost:3000'
+  axios.defaults.baseURL = 'https://url-shortener-jhzs.onrender.com/'
 
   const handleSubmit = async (e) => {
     e.preventDefault()
